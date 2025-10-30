3e1cd7431a6645e828ad6e77d6c123315ef546dc
kaushikvanavada <kaushik.vanavada@gmail.com>
2025-10-30 01:41:45 +0000

    Lesson1: Practicing comments, printing variables

---
+++ lesson1/classwork/comment.py
@@ -0,0 +1,3 @@
+# Hi
+# This is a comment
+# This lets people understand your code
--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,33 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+x=26
+print (x)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+# hello
+y=8
+print (8, "Hours")
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
 
-
+fav_fruit="Mango's"
+print (fav_fruit, "are amazing")
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
+a= "Redmond"
+b= "United States of America"
+print (a)
+print (b)
 
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+c= "no pet"
+d= "no pet"
+e= "no pet"
+print (c, d, e)
